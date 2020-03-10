bf6f1c79f9e0f5740b5c1bbc589947867b0c029f
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-10 03:28:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,9 @@ def turn_count(board)
   counter = 0
   board.each do |space|
     if space == "X" || space == "O"
-      
+      counter += 1
+    else
+      turn_count(board)
     end
-    counter += 1
   end
 end
