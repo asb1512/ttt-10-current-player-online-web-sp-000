6e3c3aad7e054b6ba3b3b977494a1ba9f150d0bd
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-10 03:44:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -18,10 +18,10 @@ end
 #   end
 # end
 
-def current_player(board)
-  if turn_count(board).even?
-    "X"
-  elsif turn_count(board).odd?
-    "O"
-  end
-end
+# def current_player(board)
+#   if turn_count(board).even?
+#     "X"
+#   elsif turn_count(board).odd?
+#     "O"
+#   end
+# end
